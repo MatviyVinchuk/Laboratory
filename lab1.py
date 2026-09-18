51c5ad9560a0db08facc55d16f27f95f9ec48ea6
MatviyVinchuk <otmenar@gmail.com>
2026-09-18 10:25:06 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+num = 6
+num1 = 1.2
+name = ("Ivan")
+bool1 = True
+numbers = [7,8,3]
+tuple1 = (10,20)
+student = {"name": "степан", "age": "11"}
+set1= {1,2,3}
+
+print(num,type(num))
+print(num1,type(num1))
+print(name,type(name))
+print(bool1,type(bool1))
+print(numbers,type(numbers))
+print(tuple1,type(tuple1))
+print(student,type(student))
+print(set1,type(set1))
+
+A = 10
+B = 5
+print (A+B)
+print (A-B)
+print (A/B)
+print (A*B)
+print (A%B)
+print (A//B)
+print (A**B)
